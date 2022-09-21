115692eb3d07a02c3dda7d0a809e25d7ea1f78a2
Yurii <109270651+humennyi@users.noreply.github.com>
2022-09-21 14:14:35 +0300

    less 3 Forms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import {Cars} from "./components";
 import './App.css';
 
 function App() {
 
-  let title ='hello okten'
+
 
   return (
     <div >
-<h2>{title}</h2>
+      <Cars/>
+
     </div>
   );
 }
